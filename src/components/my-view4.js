501c109b30038f31959384ee41a9265903bf748e
Trupti Baldaniya <baldaniyatrupti@gmail.com>
2019-10-02 12:25:54 +0530

    Add dw-icon and dw-dialog for practices

--- src/components/my-view4.js
+++ src/components/my-view4.js
@@ -1,9 +1,13 @@
 import { LitElement, html, css} from 'lit-element';
+import '@dreamworld/dw-icon-button';
+import { flexLayout } from '@dreamworld/flex-layout';
+import '@dreamworld/dw-dialog';
 
 class MyView4 extends LitElement {
 
   static get styles(){
     return[
+      flexLayout,
       css`
         :host{
           display: block;
@@ -12,15 +16,23 @@ class MyView4 extends LitElement {
         :host[hidden]{
           display: none;
         }
+        .left-align{
+          align-items: flex-start;
+        }
       `
     ]
   }
-  
+
   render() {
     return html`
+
       <h4>my view 4</h4>
+      <div class="layout vertical left-align">
+        <dw-icon-button icon="school" iconSize="32"></dw-icon-button>
+      </div>
+      <dw-dialog id="alert">Hello</dw-dialog>
     `;
   }
 }
 
-customElements.define('my-view4', MyView4);
+window.customElements.define('my-view4', MyView4);
